b43f3f190a24b64d5116d2c95ef01b1565cc464a
Giovanni Aguirre <giovanni.fi05@gmail.com>
2016-02-25 11:33:05 -0600

    :art: App themed like metro apps | A few details pending

--- src/me/learning/javafx/contacts/MainApp.java
+++ src/me/learning/javafx/contacts/MainApp.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -41,6 +42,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Contacts App");
+        this.primaryStage.getIcons().add(new Image("file:resources/img/ic_contacts_flat.png"));
 
         initRootLayout();
         showPersonOverview();
